d7cd2c5bb056a81573ca745c629f956b2f8d10e5
和泉田済人 <sumito@izumita.com>
2019-12-10 20:08:17 +0900

    ・不要なimportを消した ・importできていないエラーを直した ・機能の改善 ・PEP8に基づくコードの改行数などの変更

--- cogs/snowflake.py
+++ cogs/snowflake.py
@@ -1,20 +1,21 @@
 import discord
-import datetime
+from discord.ext import commands
+
 
 class SnowFlake(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def timestamp(self,ctx,id:int):
+    async def timestamp(self, ctx, id: int):
         """指定されたIDから時刻情報を取得します。
         IDには、役職やメッセージ、チャンネルなどのIDを指定できます。"""
-        if len(str(id)) != 18:
+        if not 17 <= len(str(id)) <= 18:
             await ctx.send("IDが不正です。")
             return
 
-        obj = discord.Object(id)
-        await ctx.send(obj.created_at)
+        await ctx.send(discord.utils.snowflake_time(id))
+
 
 def setup(bot):
     return bot.add_cog(SnowFlake(bot))
